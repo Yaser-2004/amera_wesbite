763cc27aab96864ca4ad6fb20df68a4a3f48fcd8
Yash Goyal <yashgoyalambk@gmail.com>
2025-02-24 22:18:28 +0530

    changed bg color of hero cards

--- src/app/Sections/Hero.jsx
+++ src/app/Sections/Hero.jsx
@@ -5,9 +5,10 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { ArrowUpRight } from 'lucide-react';
-
+import * as motion from "motion/react-client"
 const Hero = () => {
   return (
+    
     <div className='hero relative bg-[url(/Assets/backImage.png)] h-[1000px] bg-no-repeat bg-[center_top_-10rem] bg-cover'>
        <div className='relative ml-auto mr-auto max-w-[1300px] h-full'>
         <div className='text-center'>
@@ -21,21 +22,26 @@ const Hero = () => {
                 Start Your Search <ArrowUpRight />
             </Button>
         </div>
-
-        <div className='absolute top-[70%] left-0 bg-blue-950 w-[400px] p-10 rounded-lg border-2 border-[#250556] backdrop-blur-md bg-opacity-50 text-[#DBC2FF] text-sm'>
+        {/* <motion.div initial={{translateY:100}} whileInView={{translateY:0}} viewport={{amount:"some",once:true}} transition={{duration:1}}> */}
+        <div className='absolute top-[70%] left-0 bg-[#250556] w-[400px] p-10 rounded-lg  border-[#250556] backdrop-blur-md bg-opacity-30 text-[#DBC2FF] text-sm'>
           <p>- We specialize in securing high-value project funding (₹10Cr+) through government banks and private equity investors.</p>
           <br/>
           <p>- Our expertise spans industries like hospitality, healthcare, energy, agriculture, real estate, education, and manufacturing.</p>
         </div>
+       
 
-        <div className='absolute top-[50%] right-0 bg-blue-950 w-[400px] p-10 rounded-lg border-2 border-[#250556] backdrop-blur-md bg-opacity-50 text-[#DBC2FF] text-sm'>
+        {/* <motion.div initial={{translateY:100}} whileInView={{translateY:0}} viewport={{amount:"some",once:true}} transition={{duration:1}}> */}
+        <div className='absolute top-[50%] right-0 bg-[#250556] w-[400px] p-10 rounded-lg border-[#250556] backdrop-blur-md bg-opacity-30 text-[#DBC2FF] text-sm'>
           <p>- We also arrange LCs and BGs for Import-Export transactions above ₹10Cr through Banks Only and above 1 Cr without property from International Bank.</p>
           <br/>
           <p>- We are based at Lucknow and handling PAN India.</p>
         </div>
+        {/* /</motion.div> */}
        </div>
        <div className="absolute inset-x-0 bottom-0 h-40 bg-gradient-to-b from-transparent to-[#250556]"></div>
     </div>
+    
+   
   )
 }
 
